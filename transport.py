686f6e56f071084f20d6c2cea773e1c73de700ea
Danchik <55001277+Danchikon@users.noreply.github.com>
2020-09-28 17:57:56 +0300

    Add files via upload
    Клас для транспорта

---
+++ transport.py
@@ -0,0 +1,11 @@
+class Transport:
+    def __init__(self, longitude, latitude, maxSpeed, startSpeed=0, type_="car"):
+        self.__longitude = longitude
+        self.__latitude = latitude
+        self.__type_ = type_
+        self.__direction = {"from": None,"to": None}
+        self.__speed = startSpeed
+        self.__maxSpeed = maxSpeed
+
+    def move(self):
+        pass
